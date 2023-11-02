79db0494ebe555fb61e19cbdc30e2f9f1b0b36c1
adriannovosel <134148571+adriannovosel@users.noreply.github.com>
2023-11-02 16:32:17 +0100

    Update tt_um_adriannovosel_top.v

--- src/tt_um_adriannovosel_top.v
+++ src/tt_um_adriannovosel_top.v
@@ -12,7 +12,7 @@ module tt_um_adriannovosel_top(input  wire [7:0] ui_in,    // Dedicated inputs
 
     top top1(.reset(ui_in[0]), .clk(ui_in[1]), .ch1(ui_in[2]), .ch2(ui_in[3]), .ch3(ui_in[4]), .ch4(ui_in[5]), .RTC(ui_in[6]), 
              .serial_out(uo_out[0]), .ovf_global(uo_out[1]), .ovf_RTC_out(uo_out[2]), .a0_out(uo_out[3]), .a1_out(uo_out[4]), 
-             .a2_out(uo_out[5]), .a3_out(uo_out[6]), .ovf_ch_out(uo_out[7]));
+             .a2_out(uo_out[5]), .SL_out(uo_out[6]), .ovf_ch_out(uo_out[7]));
     assign uio_out = 8'b0;
     assign uio_oe = 8'b0;
 endmodule // top
